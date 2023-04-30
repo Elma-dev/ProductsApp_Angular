c546a61cf4597169f7d3d3bc8f1d586a5c989400
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-30 01:49:04 +0100

    commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit{
+
+  customers! :Array<any>;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.customers=[
+      {id:1,name:'Customer1',age:18},
+      {id:2,name:'Customer2',age:18},
+      {id:3,name:'Customer3',age:18},
+      {id:4,name:'Customer4',age:18},
+      {id:5,name:'Customer5',age:18},
+    ]
+
+  }
+
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+
+  products! :Array<any>;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.products=[
+      {id:1,name:"Computer",price:12332},
+      {id:2,name:"Printer",price:4330},
+      {id:3,name:"Smart Phone",price:8233}
+    ]
+  }
 
 }
